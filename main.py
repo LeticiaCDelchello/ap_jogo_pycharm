35cdee790afa50f48178c2cc238f75c9687ed66f
LeticiaDelchello <leticia.delchello@gmail.com>
2025-03-09 14:06:28 -0300

    Fazendo e configurando a primeira classe Game.py

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import pygame as pg
+from code.Game import Game
 
-print('Setup Start')
-pg.init()
-window = pg.display.set_mode(size=(800, 600))
-print('Setup End')
-
-print('Loop Start')
-while True:
-    # Check for all events
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit() # Close Window
-            quit() # End pygame
+game = Game()
+game.run()
 
 
